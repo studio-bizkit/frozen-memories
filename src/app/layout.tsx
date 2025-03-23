a6176341f05442154752001b69e73addf10513c8
unknown <umail2bilal@gmail.com>
2025-03-23 10:14:22 +0530

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto_Serif, Roboto } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const robotoSerif = Roboto_Serif({
   variable: "--font-roboto-serif",
@@ -37,6 +38,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${robotoSerif.variable} ${roboto.variable} antialiased`}
       >
+        <Analytics/>
         {children}
       </body>
     </html>
